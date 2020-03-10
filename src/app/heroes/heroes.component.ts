3b592e70539d644d7720ff9d4e2dfbd6ed48b162
haoxinDong <haoxinDong1217@outlook.com>
2020-03-10 21:53:59 +0800

    Merge branch 'master' of https://github.com/haoxinDong/angular-tour-of-heroes
    # Conflicts:
    #	README.md

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import  { Hero } from '../Hero';
-import  {HEROES} from '../mock-heroes';
-
-
+import {Component, OnInit} from '@angular/core';
+import {Hero} from '../Hero';
+import { HeroService } from '../hero.service';
+// import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -10,14 +9,30 @@ import  {HEROES} from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero : Hero =  {
-    id: 1,
-    name: 'Windstorm'
-  };
 
-  constructor() { }
+  heroes: Hero[];
+
+  selectedHero: Hero;
+
+  constructor(
+    private heroService: HeroService,
+    // private messageService: MessageService
+  ) {
+  }
+
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+
+
 
   ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
